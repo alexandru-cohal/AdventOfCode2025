f7a82f648f1ae0566ddfcd6ef8ed6249768cd99d
Alexandru Cohal <alexandru.cohal@yahoo.com>
2026-04-12 20:49:35 +0200

    Add day 11 solution for part 2. Rework day 11 solution for part 1 to use the same function as the solution for part 2

--- day11.cpp
+++ day11.cpp
@@ -3,12 +3,11 @@
 #include <sstream>
 #include <vector>
 #include <map>
-#include <set>
-#include <queue>
 using namespace std;
 
-map<string, vector<string>> Graph, GraphReverse;
-map<string, long long> NumPath;
+map<string, vector<string>> Graph;
+map<string, long long> PathCount;
+map<string, bool> NodeVisited;
 
 void ReadFile(string Filename)
 {
@@ -19,120 +18,106 @@ void ReadFile(string Filename)
     {
         stringstream LineStream(Line);
         string NodeSrc, NodeDest;
-
+        vector<string> NodeDestVector;
         getline(LineStream, NodeSrc, ':');
-        Graph[NodeSrc] = {};
-
         getline(LineStream, NodeDest, ' '); // Discard the semicolon and the space after NodeSrc
-
         while (getline(LineStream, NodeDest, ' '))
         {
-            Graph[NodeSrc].push_back(NodeDest);
-
-            if (GraphReverse.find(NodeDest) == GraphReverse.end())
-            {
-                GraphReverse[NodeDest] = {};
-            }
-            GraphReverse[NodeDest].push_back(NodeSrc);
-
-            NumPath[NodeSrc] = -1;
+            NodeDestVector.push_back(NodeDest);
         }
+        Graph[NodeSrc] = NodeDestVector;
+        PathCount[NodeSrc] = -1;
+        NodeVisited[NodeSrc] = false;
     }
 }
 
-void GetReachable(string Node, map<string, vector<string>> Graph, set<string> &NodeReachable)
+void CleanHelperVariables()
 {
-    queue<string> BfsQueue = {};
-    BfsQueue.push(Node);
+    // Reset the values of all the keys from the PathCount map
+    for (auto ItPathCount = PathCount.begin(); ItPathCount != PathCount.end(); ++ItPathCount) 
+        ItPathCount->second = -1;
 
-    while (BfsQueue.size())
-    {
-        string NodeCurr = BfsQueue.front();
-        BfsQueue.pop();
-
-        NodeReachable.insert(NodeCurr);
-
-        for (int IdxConn = 0; IdxConn < Graph[NodeCurr].size(); IdxConn++)
-        {
-            string NodeConn = Graph[NodeCurr][IdxConn];
-            if (NodeReachable.find(NodeConn) == NodeReachable.end())
-            {
-                BfsQueue.push(NodeConn);
-            }
-        }
-    }
+    // Reset the values of all the keys from the NodeVisited map
+    for (auto ItNodeVisited = NodeVisited.begin(); ItNodeVisited != NodeVisited.end(); ++ItNodeVisited) 
+        ItNodeVisited->second = false;
 }
 
-long long CalculatePath(string NodeCurr, string NodeDest, map<string, vector<string>> Graph, set<string> NodeReachable)
+/**
+ * Graph Traversal: 
+ *  - Backtracking DFS 
+ *  - Storing visited nodes (for avoiding cycles) 
+ *  - Memoization of calculated paths (for avoiding recomputing the same values)
+ * */ 
+long long TraverseGraph(string NodeSrc, string NodeDest)
 {
-    if (NodeCurr == NodeDest)
+    // Corner cases
+    if (NodeSrc == NodeDest)
     {
+        // The base case when source node is the same as destination node.
+        // Then there is only one possibility.
         return 1;
     }
-    else
+    if (NodeVisited[NodeSrc] == true)
     {
-        if (NumPath[NodeCurr] == -1)
-        {
-            NumPath[NodeCurr] = 0;
-            for (int IdxConn = 0; IdxConn < Graph[NodeCurr].size(); IdxConn++)
-            {
-                string NodeConn = Graph[NodeCurr][IdxConn];
-                if (NodeReachable.find(NodeConn) != NodeReachable.end())
-                {
-                    NumPath[NodeCurr] += CalculatePath(NodeConn, NodeDest, Graph, NodeReachable);
-                }
-            }
-        }
-        return NumPath[NodeCurr];
+        // The node was already visited which means that a cycle was detected.
+        // Then ignore this path in order to avoid endless loops in the cycle.
+        return 0;
     }
-}
-
-void ResetNumPath()
-{
-    for (auto NumPathElem : NumPath)
+    if (NodeSrc == "out")
     {
-        NumPathElem.second = -1;
+        // Node "out" is not the source of any connection.
+        // Then for sure there is no possibility to get from "out" to any other node.
+        return 0;
     }
-}
+    if (PathCount[NodeSrc] != -1)
+    {
+        // The node was not already visited but the number of paths was already calculated.
+        // So use the already calculated value. 
+        return PathCount[NodeSrc];
+    }
+    
+    // No corner case was detected => We are in the basic situation.
+    // Use the source node as step on the overall path.
+    // Mark the source node as visited, go through all its connected nodes and call the traversing function, 
+    // sum up all the returned number of paths, unmark the source node as visited.
+    NodeVisited[NodeSrc] = true;
+    long long PathCountNodeSrc = 0;
+    for(int IdxConn = 0; IdxConn < Graph[NodeSrc].size(); IdxConn++)
+    {
+        string NodeConn = Graph[NodeSrc][IdxConn];
+        PathCountNodeSrc += TraverseGraph(NodeConn, NodeDest);
+    }
+    NodeVisited[NodeSrc] = false;
+    PathCount[NodeSrc] = PathCountNodeSrc;
 
-long long SolvePart2()
-{
-    set<string> NodeReachableFft = {};
-    GetReachable("fft", GraphReverse, NodeReachableFft);
-    ResetNumPath();
-    long long NumSvr2Fft = CalculatePath("svr", "fft", Graph, NodeReachableFft);
-    cout << "svr 2 fft: " << NumSvr2Fft << endl;
-    ResetNumPath();
-    long long NumDac2Fft = CalculatePath("dac", "fft", Graph, NodeReachableFft);
-    cout << "dac 2 fft: " << NumDac2Fft << endl;
-
-    set<string> NodeReachableDac = {};
-    GetReachable("dac", GraphReverse, NodeReachableDac);
-    ResetNumPath();
-    long long NumSvr2Dac = CalculatePath("svr", "dac", Graph, NodeReachableDac);
-    cout << "svr 2 dac: " << NumSvr2Dac << endl;
-    ResetNumPath();
-    long long NumFft2Dac = CalculatePath("fft", "dac", Graph, NodeReachableDac);
-    cout << "fft 2 dac: " << NumFft2Dac << endl;
-
-    set<string> NodeReachableOut = {};
-    GetReachable("dac", GraphReverse, NodeReachableOut);
-    ResetNumPath();
-    long long NumFft2Out = CalculatePath("fft", "out", Graph, NodeReachableOut);
-    cout << "fft 2 out: " << NumFft2Out << endl;
-    ResetNumPath();
-    long long NumDac2Out = CalculatePath("dac", "out", Graph, NodeReachableOut);
-    cout << "dac 2 out: " << NumDac2Out << endl;
-
-    return (NumSvr2Fft * NumFft2Dac * NumDac2Out) + (NumSvr2Dac * NumDac2Fft * NumFft2Out);
+    return PathCountNodeSrc;
 }
 
 int main()
 {
     ReadFile("input/day11.txt");
 
-    //cout << SolvePart1() << endl;
-    cout << SolvePart2() << endl;
+    // Solve Part 1
+    cout << TraverseGraph("you", "out") << endl;
+
+    // Solve Part 2
+    CleanHelperVariables();
+    long long PathCountSvrFft = TraverseGraph("svr", "fft");
+    CleanHelperVariables();
+    long long PathCountFftDac = TraverseGraph("fft", "dac");
+    CleanHelperVariables();
+    long long PathCountDacOut = TraverseGraph("dac", "out");
+    long long PathCountSvrFftDacOut = PathCountSvrFft * PathCountFftDac * PathCountDacOut;
+
+    CleanHelperVariables();
+    long long PathCountSvrDac = TraverseGraph("svr", "dac");
+    CleanHelperVariables();
+    long long PathCountDacFft = TraverseGraph("dac", "fft");
+    CleanHelperVariables();
+    long long PathCountFftOut = TraverseGraph("fft", "out");
+    long long PathCountSvrDacFftOut = PathCountSvrDac * PathCountDacFft * PathCountFftOut;
+
+    cout << PathCountSvrFftDacOut + PathCountSvrDacFftOut << endl;
 
     return 0;
 }
