0f9a3b545239fe00758380b35919544c90899935
Alexandru Cohal <alexandru.cohal@yahoo.com>
2026-04-19 21:00:24 +0200

    Add day 9 solution for part 2. Refactor and add comments for day 9 solution for part 1. Delete extra line at the end of day 9 input file

--- day9.cpp
+++ day9.cpp
@@ -6,9 +6,6 @@
 #include <chrono>
 using namespace std;
 
-const int STEP_LINE[4] = {0, -1, 0, 1};
-const int STEP_COL[4] = {-1, 0, 1, 0}; 
-
 vector<pair<int, int>> ReadFileLineByLine(string Filename)
 {
     ifstream InputFile(Filename);
@@ -16,11 +13,13 @@ vector<pair<int, int>> ReadFileLineByLine(string Filename)
     vector<pair<int, int>> RedTiles;
     pair<int, int> Coordinates;
 
-    while (getline(InputFile, Line, ','))
+    while (getline(InputFile, Col, ','))
     {
-        getline(InputFile, Col);
-        Coordinates.second = stoi(Line);
-        Coordinates.first = stoi(Col);
+        getline(InputFile, Line);
+
+        Coordinates.first = stoi(Line);
+        Coordinates.second = stoi(Col);
+
         RedTiles.push_back(Coordinates);
     }
 
@@ -31,6 +30,7 @@ long long SolvePart1(vector<pair<int, int>> RedTiles)
 {
     long long MaxArea = 0;
 
+    // For every combination of 2 red tiles, calculate the area of the rectangle determined by them and find the maximum area
     for (int IdxRedTile1 = 0; IdxRedTile1 < RedTiles.size() - 1; IdxRedTile1++)
     {
         for (int IdxRedTile2 = IdxRedTile1 + 1; IdxRedTile2 < RedTiles.size(); IdxRedTile2++)
@@ -38,98 +38,134 @@ long long SolvePart1(vector<pair<int, int>> RedTiles)
             int DiffLine = abs(RedTiles[IdxRedTile1].first - RedTiles[IdxRedTile2].first) + 1;
             int DiffCol = abs(RedTiles[IdxRedTile1].second - RedTiles[IdxRedTile2].second) + 1;
             long long Area = (long long)DiffLine * DiffCol;
-            if (Area > MaxArea)
-            {
-                MaxArea = Area;
-            }
+
+            MaxArea = max(MaxArea, Area);
         }
     }
 
     return MaxArea;
 }
 
-typedef enum
+bool CheckPointInsidePolygon(int PointLine, int PointCol, vector<pair<int, int>> NodePolygon)
 {
-    DIR_HORIZ = 0,
-    DIR_VERT_DESC,
-    DIR_VERT_ASC
-} DirectionType;
-
-typedef struct
-{
-    pair<int, int> Point1;
-    pair<int, int> Point2;
-    DirectionType Direction;
-} SegmentType;
-
-bool InsidePolygon(int PointLine, int PointCol, vector<SegmentType> Segments)
-{
-    int NumIntersections = 0;
-    bool OnEdge = false;
-
     /**
-     * Ray casting algorithm:
-     * Consider a horizontal ray (line = PointLine). 
-     * Check only the vertical edges of the polygon.
-     * Check only the vertical edges on a column to the right of the PointCol.
-     * If the ray intersects the limit of an edge => Consider it.
+     * Check 1: 
+     *      - If the point is on any vertical / horizontal edge => Classify the point as inside the polygon
+     *      - If the point is on any vertical / horizontal edge => Move to Check 2
+     * Check 2: Apply Ray Casting Algorithm
+     *      - Consider a horizontal ray starting from the point to the right side
+     *      - Count how many vertical edges this ray intersects
+     *          - For avoining double counting, strict check for edge's first point
+     *      - If the number of intersections is odd => Classify the point as inside the polygon
+     *      - If the number of intersections is even => Classify the point as outside the polygon
      */
 
-    for (int IdxSegment = 0; IdxSegment < Segments.size(); IdxSegment++)
-    {
-        if (Segments[IdxSegment].Direction == DIR_VERT_ASC || Segments[IdxSegment].Direction == DIR_VERT_DESC)
-        {
-            // Vertical Edge
-            int EdgeLineMin = min(Segments[IdxSegment].Point1.first, Segments[IdxSegment].Point2.first);
-            int EdgeLineMax = max(Segments[IdxSegment].Point1.first, Segments[IdxSegment].Point2.first);
+    int NumEdgeIntersections = 0;
 
-            if ((PointCol == Segments[IdxSegment].Point1.second) && (EdgeLineMin <= PointLine) && (PointLine <= EdgeLineMax))
+    // Iterate over the polygon nodes and create the polygon edges as the pair of points (EdgeLine1, EdgeCol1) and (EdgeLine2, EdgeCol2)
+    for (int IdxNodePolygon = 0; IdxNodePolygon < NodePolygon.size(); IdxNodePolygon++)
+    {
+        int EdgeLine1, EdgeCol1, EdgeLine2, EdgeCol2;
+        EdgeLine1 = NodePolygon[IdxNodePolygon].first;
+        EdgeCol1 = NodePolygon[IdxNodePolygon].second;
+        if (IdxNodePolygon < NodePolygon.size() - 1)
+        {
+            EdgeLine2 = NodePolygon[IdxNodePolygon + 1].first;
+            EdgeCol2 = NodePolygon[IdxNodePolygon + 1].second;
+        }
+        else
+        {
+            // Consider the edge between the last node from the polygon list and the first one
+            EdgeLine2 = NodePolygon[0].first;
+            EdgeCol2 = NodePolygon[0].second;
+        }
+
+        if (EdgeCol1 == EdgeCol2)
+        {
+            // Vertical edge
+
+            // Check if the point is exactly on the edge
+            if ((PointCol == EdgeCol1) && (min(EdgeLine1, EdgeLine2) <= PointLine) && (PointLine <= max(EdgeLine1, EdgeLine2)))
             {
-                // Exactly on the edge
-                OnEdge = true;
-                break;
+                // The point is on the edge => Classify the point directly as inside the polygon
+                return true;
             }
-            else
+
+            // Check if the ray from the point towards the right side intersects the edge (strict check for edge's first point for avoiding double counting)
+            if ((PointCol < EdgeCol1) && (min(EdgeLine1, EdgeLine2) < PointLine) && (PointLine <= max(EdgeLine1, EdgeLine2)))
             {
-                // Not on the edge. Check if the ray intersects it
-                if (PointCol < Segments[IdxSegment].Point1.second)
-                {
-                    if ((EdgeLineMin < PointLine) && (PointLine < EdgeLineMax))
-                    {
-                        // Intersection in the inner part of the edge
-                        NumIntersections++;
-                    }
-                    else if ((EdgeLineMin == PointLine) || (PointLine == EdgeLineMax))
-                    {
-                        // Intersection in one of the ends of the edge
-                        int IdxSegmentNext = (IdxSegment + 2) % Segments.size();
-                        if ((Segments[IdxSegment].Direction == DIR_VERT_DESC && Segments[IdxSegmentNext].Direction == DIR_VERT_DESC) ||
-                            (Segments[IdxSegment].Direction == DIR_VERT_ASC && Segments[IdxSegmentNext].Direction == DIR_VERT_ASC))
-                        {
-                            NumIntersections++;
-                        }
-                    }
-                }
+                // The ray from the point towards the right side intersects the edge => Increase the number of intersections
+                NumEdgeIntersections++;
             }
         }
         else
         {
             // Horizontal edge
-            int EdgeColMin = min(Segments[IdxSegment].Point1.second, Segments[IdxSegment].Point2.second);
-            int EdgeColMax = max(Segments[IdxSegment].Point1.second, Segments[IdxSegment].Point2.second);
 
-            if ((PointLine == Segments[IdxSegment].Point1.first) && (EdgeColMin <= PointCol) && (PointCol <= EdgeColMax))
+            // Check if the point is exactly on the edge
+            if ((PointLine == EdgeLine1) && (min(EdgeCol1, EdgeCol2) <= PointCol) && (PointCol <= max(EdgeCol1, EdgeCol2)))
             {
-                // Exactly on the edge
-                OnEdge = true;
-                break;
+                // The point is on the edge => Classify the point directly as inside the polygon
+                return true;
             }
+
+            // No need to check if the ray from the point towards the right side intersects the edge as this edge will be covered by one of the vertical edges (avoid double counting)
         }
     }
 
-    if ((OnEdge == true) || (NumIntersections % 2 == 1))
+    // If the number of intersections is odd => Classify the point as inside the polygon
+    if (NumEdgeIntersections % 2 == 1)
     {
         return true;
+    }
+    return false;
+}
+
+bool CheckSegmentIntersectsPolygonEdges(int Line1, int Col1, int Line2, int Col2, vector<pair<int, int>> NodePolygon)
+{
+    // Check if the segment is horizontal or vertical
+    bool SegmentHorizontal = (Line1 == Line2) ? true : false;
+
+    // Iterate over the polygon nodes and create the polygon edges as the pair of points (EdgeLine1, EdgeCol1) and (EdgeLine2, EdgeCol2)
+    for (int IdxNodePolygon = 0; IdxNodePolygon < NodePolygon.size(); IdxNodePolygon++)
+    {
+        int EdgeLine1, EdgeCol1, EdgeLine2, EdgeCol2;
+        EdgeLine1 = NodePolygon[IdxNodePolygon].first;
+        EdgeCol1 = NodePolygon[IdxNodePolygon].second;
+        if (IdxNodePolygon < NodePolygon.size() - 1)
+        {
+            EdgeLine2 = NodePolygon[IdxNodePolygon + 1].first;
+            EdgeCol2 = NodePolygon[IdxNodePolygon + 1].second;
+        }
+        else
+        {
+            // Consider the edge between the last node from the polygon list and the first one
+            EdgeLine2 = NodePolygon[0].first;
+            EdgeCol2 = NodePolygon[0].second;
+        }
+
+        // Check if the edge is horizontal or vertical
+        bool EdgeHorizontal = (EdgeLine1 == EdgeLine2) ? true : false;
+
+        // Check the intersection if the segment is horizontal and the edge is vertical
+        if ((SegmentHorizontal == true) && (EdgeHorizontal == false))
+        {
+            if ((min(Col1, Col2) < EdgeCol1) && (EdgeCol1 < max(Col1, Col2)) && 
+                (min(EdgeLine1, EdgeLine2) < Line1) && (Line1 < max(EdgeLine1, EdgeLine2)))
+            {
+                return true;
+            }
+        }
+
+        // Check the intersection if the segment is vertical and the edge is horizontal
+        if ((SegmentHorizontal == false) && (EdgeHorizontal == true))
+        {
+            if ((min(Line1, Line2) < EdgeLine1) && (EdgeLine1 < max(Line1, Line2)) && 
+                (min(EdgeCol1, EdgeCol2) < Col1) && (Col1 < max(EdgeCol1, EdgeCol2)))
+            {
+                return true;
+            }
+        }
     }
 
     return false;
@@ -138,60 +174,38 @@ bool InsidePolygon(int PointLine, int PointCol, vector<SegmentType> Segments)
 long long SolvePart2(vector<pair<int, int>> RedTiles)
 {
     long long MaxArea = 0;
-
-    vector<SegmentType> Segments;
-    RedTiles.push_back(RedTiles[0]);
-    for (int IdxRedTile = 0; IdxRedTile < RedTiles.size(); IdxRedTile++)
-    {
-        SegmentType Segment;
-        Segment.Point1 = RedTiles[IdxRedTile];
-        Segment.Point2 = RedTiles[IdxRedTile + 1];
-        if (RedTiles[IdxRedTile].first == RedTiles[IdxRedTile + 1].first)
-        {
-            Segment.Direction = DIR_HORIZ;
-        }
-        else
-        {
-            if (RedTiles[IdxRedTile].second < RedTiles[IdxRedTile + 1].second)
-            {
-                Segment.Direction = DIR_VERT_ASC;
-            }
-            else
-            {
-                Segment.Direction = DIR_VERT_DESC;
-            }
-
-        }
-        Segments.push_back(Segment);
-    }
     
-    for (int IdxRedTile1 = 0; IdxRedTile1 < RedTiles.size() - 2; IdxRedTile1++)
+    // For every combination of 2 red tiles, check if the rectangle determined by them is completely inside the polygon. 
+    // If yes, calculate the area of the rectangle and find the maximum area.
+    for (int IdxRedTile1 = 0; IdxRedTile1 < RedTiles.size() - 1; IdxRedTile1++)
     {
-        for (int IdxRedTile2 = IdxRedTile1 + 1; IdxRedTile2 < RedTiles.size() - 1; IdxRedTile2++)
+        for (int IdxRedTile2 = IdxRedTile1 + 1; IdxRedTile2 < RedTiles.size(); IdxRedTile2++)
         {
             int RectangleLineMax = max(RedTiles[IdxRedTile1].first, RedTiles[IdxRedTile2].first);
             int RectangleLineMin = min(RedTiles[IdxRedTile1].first, RedTiles[IdxRedTile2].first);
             int RectangleColMax = max(RedTiles[IdxRedTile1].second, RedTiles[IdxRedTile2].second);
             int RectangleColMin = min(RedTiles[IdxRedTile1].second, RedTiles[IdxRedTile2].second);
 
-            if (IdxRedTile1 == 65 && IdxRedTile2 == 309)
-            {
-                int stoppoint = 1;
-            }
+            /**
+             * The rectangle is inside the polygon (which can be concave) if both conditions are fulfilled:
+             *      1. All the nodes of the rectangle are inside the polygon
+             *      2. The rectangle's edges are not crossing any polygon's edges
+             */ 
 
-            if (InsidePolygon(RectangleLineMin, RectangleColMin, Segments) == true &&
-                InsidePolygon(RectangleLineMin, RectangleColMax, Segments) == true &&
-                InsidePolygon(RectangleLineMax, RectangleColMin, Segments) == true &&
-                InsidePolygon(RectangleLineMax, RectangleColMax, Segments) == true)
+            if (CheckPointInsidePolygon(RectangleLineMin, RectangleColMin, RedTiles) == true &&
+                CheckPointInsidePolygon(RectangleLineMin, RectangleColMax, RedTiles) == true &&
+                CheckPointInsidePolygon(RectangleLineMax, RectangleColMin, RedTiles) == true &&
+                CheckPointInsidePolygon(RectangleLineMax, RectangleColMax, RedTiles) == true &&
+                CheckSegmentIntersectsPolygonEdges(RectangleLineMin, RectangleColMin, RectangleLineMin, RectangleColMax, RedTiles) == false &&
+                CheckSegmentIntersectsPolygonEdges(RectangleLineMin, RectangleColMax, RectangleLineMax, RectangleColMax, RedTiles) == false &&
+                CheckSegmentIntersectsPolygonEdges(RectangleLineMax, RectangleColMax, RectangleLineMax, RectangleColMin, RedTiles) == false &&
+                CheckSegmentIntersectsPolygonEdges(RectangleLineMax, RectangleColMin, RectangleLineMin, RectangleColMin, RedTiles) == false)
             {
-                int DiffRectangleLine = RectangleLineMax - RectangleLineMin + 1;
-                int DiffRectangleCol = RectangleColMax - RectangleColMin + 1;
-                long long AreaRectangle = (long long)DiffRectangleLine * DiffRectangleCol;
+                int DiffLine = RectangleLineMax - RectangleLineMin + 1;
+                int DiffCol = RectangleColMax - RectangleColMin + 1;
+                long long Area = (long long)DiffLine * DiffCol;
 
-                if (AreaRectangle > MaxArea)
-                {
-                    MaxArea = AreaRectangle;
-                }
+                MaxArea = max(MaxArea, Area);
             }
         }
     }
